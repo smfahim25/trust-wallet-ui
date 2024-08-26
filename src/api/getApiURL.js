100a134b17372d8b14406b2b85a5256779757579
smfahim25 <smfahimhossen@gmail.com>
2024-08-26 23:08:05 +0600

    api url updated

--- src/api/getApiURL.js
+++ src/api/getApiURL.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL = "https://cryptotradeapi.solvyxx.com/api/v1";
 // const API_BASE_URL = "https://metaadmin.online/api/v1";
-const API_BASE_URL = "http://191.96.31.155:5000/api/v1";
+const API_BASE_URL = "https://proit99.online/api/v1";
 
 // const API_BASE_URL = "http://localhost:5000/api/v1";
 
