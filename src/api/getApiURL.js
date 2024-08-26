2172f1c3134eb0fffebe0feaf3525ae7aa97d912
smfahim25 <smfahimhossen@gmail.com>
2024-08-26 18:02:56 +0600

    api url updated

--- src/api/getApiURL.js
+++ src/api/getApiURL.js
@@ -1,5 +1,7 @@
 // const API_BASE_URL = "https://cryptotradeapi.solvyxx.com/api/v1";
-const API_BASE_URL = "https://metaadmin.online/api/v1";
+// const API_BASE_URL = "https://metaadmin.online/api/v1";
+const API_BASE_URL = "http://191.96.31.155:5000/api/v1";
+
 // const API_BASE_URL = "http://localhost:5000/api/v1";
 
 export default API_BASE_URL;
